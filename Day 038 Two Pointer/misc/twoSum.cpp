f7bbd0fe7830a31bcfeb39eb0a4819c666c6776f
Parvej Alam <parvejprv1@gmail.com>
2024-03-18 17:49:31 +0530

    Two Sum - II (167)

--- Day 038 Two Pointer/misc/twoSum.cpp
+++ Day 038 Two Pointer/misc/twoSum.cpp
@@ -1,3 +1,12 @@
+
+/*  
+Solution 1. Brute force approach
+---------------------------------
+TC: O(n^2)
+SC Auxiliary: O(1)
+
+*/
+
 #include<iostream>
 #include<vector>
 
@@ -5,18 +14,24 @@ using namespace std;
 
 int main() {
   int n, target;
+
+  // size
   cout << "Enter the size of the array:";
   cin >> n;
-  cout << "Enter the target:";
-  cin >> target;
 
   vector<int> arr(n);   // create an vector
-  vector<int> ans(n);   // ans vector
-  cout << "Enter elements of the array"<< endl;
+  vector<int> ans;   // ans vector
+
+  // elements inputed to vector
+  cout << "Input the vector elements:";
   for(int i=0; i<n; i++){
     cin >> arr[i];
   }
+  // target
+  cout << "Enter the target element:";
+  cin >> target;
 
+  // two-sum (bruteforce approach)
   for(int i=0; i<n-1; i++){
     for(int j=i+1; j<n; j++){
       if((arr[i]+arr[j]) == target){
@@ -26,10 +41,27 @@ int main() {
       }
     }
   }
-
-  // for(int i=0; i<ans.size(); i++){
-  //   cout << ans[i] << " ";
-  // }
+  // display the resultant element
+  for(int i=0; i<ans.size(); i++){
+    cout << ans[i] << " ";
+  }
 
   return 0;
 }
+
+
+
+
+
+/* 
+TC: O(n^2)
+Aux SC: O(1) - no extra spaces are used here
+ 
+Output:-
+-----------
+Enter the size of the array:5
+Input the vector elements:2 7 11 15 27
+Enter the target element:22
+7 15
+
+*/
