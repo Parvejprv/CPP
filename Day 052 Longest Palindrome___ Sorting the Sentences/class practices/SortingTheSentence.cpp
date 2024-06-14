26a36d08916b90c1134126ed28308337308efe11
Parvej Alam <parvejprv1@gmail.com>
2024-06-14 17:58:29 +0530

    Sorting The sentence with the given number at the end of the word

--- Day 052 Longest Palindrome___ Sorting the Sentences/class practices/SortingTheSentence.cpp
+++ Day 052 Longest Palindrome___ Sorting the Sentences/class practices/SortingTheSentence.cpp
@@ -1,8 +1,19 @@
 /*  
-Question : Sorting the Sentence of a given sentence
+Question : Sorting the Sentence of a given string(sentence between 1-9) here each word consist the number between 1-9 at the end of the word.
+
+Example:-
+----------
+Input: is2 sentence4 This1 a3
+Ouput: This is a sentence
 
 */
 
+
+
+
+
+// Solution. 0
+/*
 #include<iostream>
 #include<vector>
 using namespace std; 
@@ -63,5 +74,101 @@ int main() {
 
   return 0;
 }
+*/
 
+
+
+/*  
+Output:-
+-----------
+
+Enter a string (sentence) : is2 sentence4 this1 a3
+
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Solution. 1 
+#include<iostream>
+#include<vector>
+using namespace std;
+vector<string> sortString(string &sentence){
+  string temp;    // temporary string
+  int index = 0;
+
+  vector<string> ans(9); // resultant vector with initial size 9 (as digit 1-9)
+
+  // run a loop over the sentence size
+  while(index < sentence.size()){
+    if(sentence[index] == ' '){
+      // last character of each word
+      int pos = temp.back() - '0';
+      // cout << "pos : " <<pos << endl;
+      temp.pop_back();    // remove the last digit from each word
+      // now store that word into the ans at pos-1 index
+      ans[pos - 1] = temp;
+      temp.clear();   // after assigning word clear the temp
+    } else {
+      temp += sentence[index];
+    }
+    index++;
+  }
+
+  // handle the last word
+  if(!temp.empty()){
+    int pos = temp.back() - '0';
+    temp.pop_back();
+    ans[pos - 1] = temp;
+  }
+
+
+  // remove empty string from resultant vector
+  vector<string> finalAns;
+  for(auto &str : ans){
+    if(!str.empty())
+    finalAns.push_back(str);
+  }
+
+  return finalAns;
+
+}
+
+int main() {
+  string sentence;
+  cout << "Enter the sentence: ";
+  getline(cin, sentence);
+
+  // function call 
+  vector<string> resultant = sortString(sentence);
+  cout << "Resultant String: ";
+  for(string str : resultant){
+    cout << str << " ";
+  }
+  cout << endl;
+
+  return 0;
+}
+
+
+
+/* 
+Output:-
+-----------
+Enter the sentence: is2 sentence4 This1 a3
+
+Resultant String: This is a sentence
+
+*/
 
